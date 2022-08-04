a2156db270a7c382cea8785f8e01ead213bf0db2
Louise <grillonlouise.pro@gmail.com>
2022-08-04 14:09:45 +0200

    title query in comics route add

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.get("/comics", async (req, res) => {
   console.log(process.env.API_KEY);
   try {
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}`
+      `https://lereacteur-marvel-api.herokuapp.com/comics?apiKey=${process.env.API_KEY}&title=${req.query.title}`
     );
     res.json(response.data);
   } catch (error) {
@@ -26,9 +26,9 @@ app.get("/comics", async (req, res) => {
 
 app.get("/characters", async (req, res) => {
   try {
-    console.log(req.query.word);
+    console.log(req.query.name);
     const response = await axios.get(
-      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&name=${req.query.word}`
+      `https://lereacteur-marvel-api.herokuapp.com/characters?apiKey=${process.env.API_KEY}&name=${req.query.name}`
     );
     res.json(response.data);
   } catch (error) {
